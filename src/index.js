48815e691c4af9be6575fb27b6a67727880f73a2
Juan Gomez <juan@wovn.io>
2019-10-13 18:19:03 -0500

    Add auth

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const resolvers = {
-  Query: {
-    info() {
-      return null
-    },
-    feed(root, args, context, info) {
-      return context.prisma.links()
-    }
-  },
-  Mutation: {
-    post(parent, { url, description }, context) {
-      return context.prisma.createLink({
-        url: url,
-        description: description
-      })
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 // 3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma }
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
+  }
 })
 
 server.start({ port: 3000, endpoint: '/' }, () =>
